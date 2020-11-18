8dc420b0de75da9c6b1a0501d49efbdbd9fe38a4
Deepak Hazarika <dhazarik@oradev.oraclecorp.com>
2020-11-18 16:30:29 +0530

    added init file

---
+++ model/__init__.py
@@ -0,0 +1,1 @@
+from .mdeas import *
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='monocular',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/tfdeepnet/monocular.git',
+    license='MIT',
+    author='Deepak',
+    author_email='deep.hazar@gmail.com',
+    description='monocular depthe estimation using cnn'
+)
